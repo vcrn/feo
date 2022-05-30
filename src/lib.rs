e8cff62d9155f30694bda12c49688b3e575938d6
vcrn <79985743+vcrn@users.noreply.github.com>
2022-05-30 22:11:58 +0200

    Separate structs SystemInfo and Memory to module system_info

--- src/lib.rs
+++ src/lib.rs
@@ -1,207 +1,19 @@
 /// A simple system resource monitoring CLI tool for Linux, especially on Raspberry Pi
 /// Author: github.com/vcrn
 mod colors;
+mod system_info;
 
 use colors::Colors;
 use rounded_div::RoundedDiv;
 use std::io::Error;
 use std::process::Command;
 use std::{fs, str, thread, time};
+use system_info::{Memory, SystemInfo};
 use termion::color::{Fg, Reset, Rgb};
 
 #[cfg(test)]
 #[path = "../tests/unit_tests/test_lib.rs"]
 mod test_lib;
-
-/// Used for storing total and available memory
-struct Memory {
-    ram: f32,
-    swap: f32,
-    ram_with_unit: Option<String>,
-    swap_with_unit: Option<String>,
-}
-
-impl Memory {
-    // Creates an instance storing memory matching the search word arguments.
-    fn new(search_word_ram: &str, search_word_swap: &str) -> Memory {
-        let meminfo_content = fs::read_to_string("/proc/meminfo").expect("Could not read meminfo");
-
-        let mut meminfo_split: Vec<&str> = meminfo_content.split(&[':', ' ', '\n'][..]).collect();
-
-        meminfo_split.retain(|&x| !x.is_empty()); // Removes empty elements.
-        let index_ram_search = meminfo_split
-            .iter()
-            .position(|&r| r == search_word_ram)
-            .unwrap_or_else(|| panic!("Couldn't find position of {}", search_word_ram));
-
-        let index_swap_search = meminfo_split
-            .iter()
-            .position(|&r| r == search_word_swap)
-            .unwrap_or_else(|| panic!("Couldn't find position of {}", search_word_swap));
-
-        let ram_kib = meminfo_split[index_ram_search + 1]
-            .parse::<f32>()
-            .expect("Failed to convert ram_kib to f32");
-
-        let swap_kib = meminfo_split[index_swap_search + 1]
-            .parse::<f32>()
-            .expect("Failed to convert swap_kib to f32");
-
-        Memory {
-            ram: ram_kib,
-            swap: swap_kib,
-            ram_with_unit: None,
-            swap_with_unit: None,
-        }
-    }
-
-    // Returns available RAM and free swap
-    fn get_free() -> Memory {
-        Self::new("MemAvailable", "SwapFree")
-    }
-
-    // Returns Memory instance storing total RAM and total swap
-    fn get_total() -> Memory {
-        Self::new("MemTotal", "SwapTotal")
-    }
-
-    // Returns argument, given in kb, formatted to suitable unit.
-    fn format(mem_in_kb: f32) -> String {
-        if mem_in_kb > 1000000. {
-            format!("{:.2}Gi", mem_in_kb / 1048576.)
-        } else if mem_in_kb > 1000. {
-            format!("{:.2}Mi", mem_in_kb / 1024.)
-        } else {
-            format!("{}Ki", mem_in_kb)
-        }
-    }
-
-    // Formats RAM and Swap to suitable units, saving the result to struct's fields for cheaper usage
-    fn save_with_unit(&mut self) {
-        self.ram_with_unit = Some(Self::format(self.ram));
-        self.swap_with_unit = Some(Self::format(self.swap));
-    }
-
-    fn get_ram_with_unit(&self) -> &str {
-        self.ram_with_unit
-            .as_ref()
-            .expect("ram_with_unit is None")
-            .as_str()
-    }
-
-    fn get_swap_with_unit(&self) -> &str {
-        self.swap_with_unit
-            .as_ref()
-            .expect("swap_with_unit is None")
-            .as_str()
-    }
-}
-
-struct SystemInfo {
-    cpu_temp: f32,
-    gpu_temp: Option<f32>,
-    cpu_time: Vec<usize>,
-    mem_free: Memory,
-    uptime: f64,
-}
-
-impl SystemInfo {
-    fn new(gpu: bool, num_cpus: usize) -> SystemInfo {
-        let cpu_temp = Self::get_cpu_temp();
-        let gpu_temp = match gpu {
-            true => Some(Self::get_gpu_temp()),
-            false => None,
-        };
-        let cpu_time = Self::get_cpu_time(num_cpus);
-        let mem_free = Memory::get_free();
-        let uptime = Self::get_uptime();
-
-        SystemInfo {
-            cpu_temp,
-            gpu_temp,
-            cpu_time,
-            mem_free,
-            uptime,
-        }
-    }
-
-    /// Returns the CPU temperature
-    fn get_cpu_temp() -> f32 {
-        // Returns CPU temp * 1000
-        let cpu_temp_content = fs::read_to_string("/sys/class/thermal/thermal_zone0/temp")
-            .expect("Failed to read CPU temp");
-        let cpu_temp_1000_f32 = cpu_temp_content
-            .trim_end()
-            .parse::<f32>()
-            .expect("Could not convert CPU temp to f32");
-        cpu_temp_1000_f32 / 1000.
-    }
-
-    /// Returns the GPU temperature
-    fn get_gpu_temp() -> f32 {
-        // Returns stdout (standard output stream) and stderr (standard error stream).
-        let gpu_temp_output = Command::new("vcgencmd")
-            .arg("measure_temp")
-            .output()
-            .expect("Failed to execute command");
-        // Takes stdout (bytestring), turns to utf8, splits at '=' and ''' and saves as vector.
-        let gpu_temp_str_splitted: Vec<&str> = str::from_utf8(&gpu_temp_output.stdout)
-            .expect("Failed to convert from byte string")
-            .split(['=', '\''].as_ref())
-            .collect();
-        //  Takes second element of vector, i.e. the temperature.
-        gpu_temp_str_splitted[1]
-            .parse::<f32>()
-            .expect("Couldn't convert GPU temp to f32")
-    }
-
-    /// Returns the number of CPU(s)
-    fn get_cpus() -> usize {
-        let n_cpus_output = Command::new("nproc")
-            .output()
-            .expect("Failed to execute command");
-        // Returns the CPU temperature multiplied by 1000.
-        let n_cpus_utf8 = str::from_utf8(&n_cpus_output.stdout)
-            .expect("Failed to convert n_cpus_outputto utf8 from byte string");
-
-        n_cpus_utf8
-            .trim_end()
-            .parse::<usize>()
-            .expect("Couldn't convert n_cpus_utf8 to usize")
-    }
-
-    /// Returns the sum of cumulative CPU times for user and system since boot. TODO: Consider getting time for I/O wait?
-    fn get_cpu_time(num_cpus: usize) -> Vec<usize> {
-        let proc_stat_content =
-            fs::read_to_string("/proc/stat").expect("Failed to read cpu times from file");
-
-        let proc_stat_split: Vec<&str> = proc_stat_content.split(&[' ', '\n'][..]).collect();
-        let mut cpu_times = Vec::new();
-
-        for i in 0..num_cpus {
-            let search_word = format!("cpu{}", i);
-            let index_cpun = proc_stat_split
-                .iter()
-                .position(|&r| r == search_word)
-                .unwrap_or_else(|| panic!("Couldn't find position of {}", search_word));
-            let cpu_i_time_sum = proc_stat_split[index_cpun + 1].parse::<usize>().unwrap()
-                + proc_stat_split[index_cpun + 1 + 2]
-                    .parse::<usize>()
-                    .unwrap();
-            cpu_times.push(cpu_i_time_sum);
-        }
-        cpu_times
-    }
-
-    /// Returns uptime since boot in seconds
-    fn get_uptime() -> f64 {
-        let uptime_content = fs::read_to_string("/proc/uptime").expect("Failed to read uptime");
-        let uptime_split: Vec<&str> = uptime_content.split(&[' '][..]).collect();
-        uptime_split[0]
-            .parse::<f64>()
-            .expect("Failed to convert uptime to f64")
-    }
-}
 
 /// Basic check for compability instead of designing all corresponding functions to handle errors well.
 fn check_compability(check_gpu: bool) -> Result<(), Error> {
@@ -231,7 +43,7 @@ pub fn run(delay: usize, gpu: bool, color: char) -> Result<(), Error> {
     let mut mem_total = Memory::get_total();
     mem_total.save_with_unit();
 
-    let mut cpu_time_prev = SystemInfo::get_cpu_time(SystemInfo::get_cpus()); // Initial CPU time
+    let mut cpu_time_prev = SystemInfo::new(gpu, SystemInfo::get_cpus()).cpu_time; // Initial CPU time
 
     loop {
         let system_info = SystemInfo::new(gpu, cpu_time_prev.len());
--- tests/unit_tests/test_lib.rs
+++ tests/unit_tests/test_lib.rs
@@ -2,61 +2,11 @@ mod test_lib {
     use super::super::*;
 
     #[test]
-    fn test_get_mem_with_unit() {
-        let mut mem = Memory {
-            ram: 16000000.0,
-            swap: 100000.0,
-            ram_with_unit: None,
-            swap_with_unit: None,
-        };
-        mem.save_with_unit();
-        let correct_format_ram = format!("{:.2}Gi", mem.ram / (1024.0 * 1024.0));
-        let correct_format_swap = format!("{:.2}Mi", mem.swap / (1024.0));
+    fn test_print_comp_temp() {
+        let colors = Colors::new('s');
+        let temp = 38.0;
+        let component = "CPU";
 
-        assert_eq!(correct_format_ram, mem.get_ram_with_unit());
-        assert_eq!(correct_format_swap, mem.get_swap_with_unit());
-    }
-
-    #[test]
-    fn test_color_new_white() {
-        let colors = Colors::new('w');
-
-        assert_eq!(Rgb(255, 255, 255), colors.temp);
-        assert_eq!(Rgb(255, 255, 255), colors.cpu);
-        assert_eq!(Rgb(255, 255, 255), colors.mem);
-        assert_eq!(Rgb(255, 255, 255), colors.uptime);
-    }
-
-    #[test]
-    fn test_color_new_no_match_returns_standard() {
-        let colors_mo_match = Colors::new('x');
-        let colors_std = Colors::new('s');
-
-        assert_eq!(colors_std.temp, colors_mo_match.temp);
-        assert_eq!(colors_std.cpu, colors_mo_match.cpu);
-        assert_eq!(colors_std.mem, colors_mo_match.mem);
-        assert_eq!(colors_std.uptime, colors_mo_match.uptime);
-    }
-
-    #[test]
-    #[ignore] // Fails on VM such as Github Actions
-    fn test_get_cpu_temp() {
-        SystemInfo::get_cpu_temp();
-    }
-
-    #[test]
-    fn test_get_cpu_time() {
-        SystemInfo::get_cpu_time(SystemInfo::get_cpus());
-    }
-
-    #[test]
-    fn test_get_uptime() {
-        SystemInfo::get_uptime();
-    }
-
-    #[test]
-    #[ignore] // Fails on VM such as Github Actions
-    fn test_system_info_new_gpu_false() {
-        SystemInfo::new(false, SystemInfo::get_cpus());
+        print_comp_temp(colors.cpu, temp, component);
     }
 }
